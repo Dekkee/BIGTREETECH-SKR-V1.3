06196bfc2f6c947fb233e8e37ad3347c31b64308
Dekker <dekker25@gmail.com>
2019-11-30 13:41:38 +0300

    tune steppers

--- BTT SKR V1.3/firmware/Marlin-bugfix-2.0.x/Marlin/Configuration.h
+++ BTT SKR V1.3/firmware/Marlin-bugfix-2.0.x/Marlin/Configuration.h
@@ -727,7 +727,7 @@
  * Override with M92
  *                                      X, Y, Z, E0 [, E1[, E2...]]
  */
-#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80, 80, 400, 100 }
+#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80, 80, 400, 94.34 }
 
 /**
  * Default Max Feed Rate (mm/s)
@@ -1332,7 +1332,7 @@
 #endif
 
 // Homing speeds (mm/m)
-#define HOMING_FEEDRATE_XY (20*60)
+#define HOMING_FEEDRATE_XY (50*60)
 #define HOMING_FEEDRATE_Z  (4*60)
 
 // Validate that endstops are triggered on homing moves
